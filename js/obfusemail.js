24113a0e7e784682bd466bd69938eff31fe99010
Kenneth Douglass <kenneth.douglass@texture.com>
2018-12-04 14:36:52 -0700

    first commit

---
+++ js/obfusemail.js
@@ -0,0 +1,8 @@
+var obfusemail = function () {
+	var w = 'kennethmdouglass'; // email name
+	var x = 'gmail.com'; // email domain
+	var xx = "?subject=Grimm Inquiry"; // email subject line
+	var y = 'kennethmdouglass@gmail.com'; // link text
+	var z = document.write("<a href='mailto:"+ w + '@' + x + xx +"' class='new-win'>"+ y +"<\/a>");
+}; // obfusemail()
+
---
+++ js/sitedata.js
@@ -0,0 +1,49 @@
+var deckData = {
+	cards:  [
+		{
+			icon: '<i class="fas fa-anchor"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="far fa-bell"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="fas fa-bicycle"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="fas fa-campground"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="fas fa-cat"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="fas fa-dice"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="fas fa-fish"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="fas fa-om"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="fab fa-sass"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="fas fa-horse"></i>',
+			text: 'text'
+		},
+		{
+			icon: '<i class="fas fa-shuttle-van"></i>',
+			text: 'text'
+		}
+	]
+};
+
